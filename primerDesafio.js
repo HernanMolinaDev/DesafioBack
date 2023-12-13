a3e4f1f1c1e3a9dfb81c91e5630916fb4c8d2d4a
HernanMolinaDev <hernanjmolinadev@gmail.com>
2023-12-13 09:14:41 -0200

    commit

--- primerDesafio.js
+++ primerDesafio.js
@@ -1,77 +1,68 @@
 class ProductManager {
-    constructor() {
-        this.products = [];
-    }
+    #products = [];
 
-    generateUniqueId() {
-
-        return Math.random().toString(36).substring(2, 10);
-    }
-
-    getProducts() {
-        return this.products;
-    }
-
-    addProduct(product) {
-        const existingProduct = this.products.find((p) => p.code === product.code);
-
-        if (existingProduct) {
-            throw new Error("El código de producto ya está en uso");
-        }
-
-        const newProduct = {
-            id: this.generateUniqueId(),
-            ...product,
-        };
-
-        this.products.push(newProduct);
-
+    create(data) {
+        const { title, photo, price, stock } = data;
+        const id = this.#generateUniqueId();
+        const newProduct = { id, title, photo, price, stock };
+        this.#products.push(newProduct);
         return newProduct;
     }
 
-    getProductById(productId) {
-        const product = this.products.find((p) => p.id === productId);
+    read() {
+        return this.#products;
+    }
 
-        if (!product) {
-            throw new Error("Producto no encontrado");
-        }
+    readOne(id) {
+        return this.#products.find((product) => product.id === id);
+    }
 
-        return product;
+    #generateUniqueId() {
+        return Math.random().toString(36).substring(2, 10);
+    }
+}
+
+class UserManager {
+    #users = [];
+
+    create(data) {
+        const { name, photo, email } = data;
+        const id = this.#generateUniqueId();
+        const newUser = { id, name, photo, email };
+        this.#users.push(newUser);
+        return newUser;
+    }
+
+    read() {
+        return this.#users;
+    }
+
+    readOne(id) {
+        return this.#users.find((user) => user.id === id);
+    }
+
+    #generateUniqueId() {
+        return Math.random().toString(36).substring(2, 10);
     }
 }
 
 const productManager = new ProductManager();
+const userManager = new UserManager();
 
-console.log(productManager.getProducts());
+const newProduct = productManager.create({
+    title: "Producto 1",
+    photo: "/path/to/image",
+    price: 50,
+    stock: 10,
+});
 
+const newUser = userManager.create({
+    name: "Usuario 1",
+    photo: "/path/to/user/image",
+    email: "usuario1@example.com",
+});
 
-const newProduct = {
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-};
-
-const addedProduct = productManager.addProduct(newProduct);
-console.log("Producto agregado:", addedProduct);
-
-
-console.log(productManager.getProducts());
-
-
-try {
-    productManager.addProduct(newProduct);
-} catch (error) {
-    console.error(error.message);
-}
-
-
-const productIdToFind = addedProduct.id;
-try {
-    const foundProduct = productManager.getProductById(productIdToFind);
-    console.log("Producto encontrado por ID:", foundProduct);
-} catch (error) {
-    console.error(error.message);
-}
+console.log("Productos:", productManager.read());
+console.log("Usuarios:", userManager.read());
+console.log("Producto por ID:", productManager.readOne(newProduct.id));
+console.log("Usuario por ID:", userManager.readOne(newUser.id));
